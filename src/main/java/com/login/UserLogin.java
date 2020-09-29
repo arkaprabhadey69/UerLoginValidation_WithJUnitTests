759deccd4a76ad23dd827a1b7903903cf7284577
arkaprabhadey <euphoricdey420@gmail.com>
2020-09-29 14:40:59 +0530

    [Arka]Added:Welcome Message

---
+++ src/main/java/com/login/UserLogin.java
@@ -0,0 +1,9 @@
+package com.login;
+
+public class UserLogin {
+
+    public void ShowWelcomeMessage()
+    {
+        System.out.println("Welcome to User Login Validation and JUnit Testing");
+    }
+}
